2401026a8c7c1fc0d263792b170eeda690c053ed
renxiaobo <renxiaobo@DESKTOP-9GV1ML6>
2017-12-01 16:56:12 +0800

    ~

--- springms-provider-user-mysql-hibernate-druid/src/main/java/study/bobo/App.java
+++ springms-provider-user-mysql-hibernate-druid/src/main/java/study/bobo/App.java
@@ -63,65 +63,10 @@ public class App {
 		return filterRegistrationBean;
 	}
 	
-	
+
 	 @Bean  
-	    public DataSource druidDataSource(@Value("${spring.datasource.url}") String url,  
-	            @Value("${spring.datasource.driver-class-name}") String driver,  
-	            @Value("${spring.datasource.username}") String userName,  
-	            @Value("${spring.datasource.password}") String password,  
-	            @Value("${spring.datasource.maxActive}") int maxActive,  
-	            @Value("${spring.datasource.filters}") String filters,  
-	            @Value("${spring.datasource.initialSize}")  
-	            int initialSize,  
-	            @Value("${spring.datasource.minIdle}")  
-	            int minIdle,  
-	            @Value("${spring.datasource.maxWait}")  
-	            int maxWait,  
-	            @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")  
-	            int timeBetweenEvictionRunsMillis,  
-	            @Value("${spring.datasource.minEvictableIdleTimeMillis}")  
-	            int minEvictableIdleTimeMillis,  
-	            @Value("${spring.datasource.validationQuery}")  
-	            String validationQuery,  
-	            @Value("${spring.datasource.testWhileIdle}")  
-	            boolean testWhileIdle,  
-	            @Value("${spring.datasource.testOnBorrow}")  
-	            boolean testOnBorrow,  
-	            @Value("${spring.datasource.testOnReturn}")  
-	            boolean testOnReturn,  
-	            @Value("${spring.datasource.poolPreparedStatements}")  
-	            boolean poolPreparedStatements,  
-	            @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")  
-	            int maxPoolPreparedStatementPerConnectionSize,  
-	            @Value("${spring.datasource.connectionProperties}")  
-	            String connectionProperties,  
-	            @Value("${spring.datasource.useGlobalDataSourceStat}")  
-	            boolean useGlobalDataSourceStat  
-	  
-	    ) {  
+	    public DataSource druidDataSource(@Value("${spring.datasource.filters}") String filters) {  
 	        DruidDataSource dataSource = new DruidDataSource();  
-	        /*数据源主要配置*/  
-	        dataSource.setUrl(url);  
-	        dataSource.setDriverClassName(driver);  
-	        dataSource.setUsername(userName);  
-	        dataSource.setPassword(password);  
-	          
-	        /*数据源补充配置*/  
-	        dataSource.setMaxActive(maxActive);  
-	        dataSource.setInitialSize(initialSize);  
-	        dataSource.setMinIdle(minIdle);  
-	        dataSource.setMaxWait(maxWait);  
-	        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);  
-	        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);  
-	        dataSource.setValidationQuery(validationQuery);  
-	        dataSource.setTestOnBorrow(testOnBorrow);  
-	        dataSource.setTestOnReturn(testOnReturn);  
-	        dataSource.setTestWhileIdle(testWhileIdle);  
-	        dataSource.setPoolPreparedStatements(poolPreparedStatements);  
-	        dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);  
-	        dataSource.setConnectionProperties(connectionProperties);  
-	        dataSource.setUseGlobalDataSourceStat(useGlobalDataSourceStat);  
-	          
 	        try {  
 	            dataSource.setFilters(filters);  
 	            System.err.println("Druid数据源初始化设置成功......");
@@ -132,4 +77,74 @@ public class App {
 	        return dataSource;  
 	  
 	    }  
+	
+//	
+//	 @Bean  
+//	    public DataSource druidDataSource(@Value("${spring.datasource.url}") String url,
+//	            @Value("${spring.datasource.driver-class-name}") String driver,  
+//	            @Value("${spring.datasource.username}") String userName,  
+//	            @Value("${spring.datasource.password}") String password,  
+//	            @Value("${spring.datasource.maxActive}") int maxActive,  
+//	            @Value("${spring.datasource.filters}") String filters,  
+//	            @Value("${spring.datasource.initialSize}")  
+//	            int initialSize,  
+//	            @Value("${spring.datasource.minIdle}")  
+//	            int minIdle,  
+//	            @Value("${spring.datasource.maxWait}")  
+//	            int maxWait,  
+//	            @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")  
+//	            int timeBetweenEvictionRunsMillis,  
+//	            @Value("${spring.datasource.minEvictableIdleTimeMillis}")  
+//	            int minEvictableIdleTimeMillis,  
+//	            @Value("${spring.datasource.validationQuery}")  
+//	            String validationQuery,  
+//	            @Value("${spring.datasource.testWhileIdle}")  
+//	            boolean testWhileIdle,  
+//	            @Value("${spring.datasource.testOnBorrow}")  
+//	            boolean testOnBorrow,  
+//	            @Value("${spring.datasource.testOnReturn}")  
+//	            boolean testOnReturn,  
+//	            @Value("${spring.datasource.poolPreparedStatements}")  
+//	            boolean poolPreparedStatements,  
+//	            @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")  
+//	            int maxPoolPreparedStatementPerConnectionSize,  
+//	            @Value("${spring.datasource.connectionProperties}")  
+//	            String connectionProperties,  
+//	            @Value("${spring.datasource.useGlobalDataSourceStat}")  
+//	            boolean useGlobalDataSourceStat  
+//	  
+//	    ) {  
+//	        DruidDataSource dataSource = new DruidDataSource();  
+//	        /*数据源主要配置*/  
+//	        dataSource.setUrl(url);  
+//	        dataSource.setDriverClassName(driver);  
+//	        dataSource.setUsername(userName);  
+//	        dataSource.setPassword(password);  
+//	          
+//	        /*数据源补充配置*/  
+//	        dataSource.setMaxActive(maxActive);  
+//	        dataSource.setInitialSize(initialSize);  
+//	        dataSource.setMinIdle(minIdle);  
+//	        dataSource.setMaxWait(maxWait);  
+//	        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);  
+//	        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);  
+//	        dataSource.setValidationQuery(validationQuery);  
+//	        dataSource.setTestOnBorrow(testOnBorrow);  
+//	        dataSource.setTestOnReturn(testOnReturn);  
+//	        dataSource.setTestWhileIdle(testWhileIdle);  
+//	        dataSource.setPoolPreparedStatements(poolPreparedStatements);  
+//	        dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);  
+//	        dataSource.setConnectionProperties(connectionProperties);  
+//	        dataSource.setUseGlobalDataSourceStat(useGlobalDataSourceStat);  
+//	          
+//	        try {  
+//	            dataSource.setFilters(filters);  
+//	            System.err.println("Druid数据源初始化设置成功......");
+//	        } catch (SQLException e) {  
+//	            e.printStackTrace();  
+//	            System.err.println("Druid数据源初始化设置失败......");
+//	        }  
+//	        return dataSource;  
+//	  
+//	    }  
 }
